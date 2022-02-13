d4e182f719adf7a5423be01ce05fd95e415de3e5
Switt Chawanwanidchai <switt.chaw@gmail.com>
2022-02-13 21:06:06 +0700

    add all files and example

---
+++ main.py
@@ -0,0 +1,22 @@
+from flat import Bill, Flatmate
+from report import PdfReport
+
+
+bill_amount = float(input("Input the bill amount: "))
+pay_period = input("Input the bill period? E.g. December 2020: ")
+
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many did {name1} stay in the house during the bill period? "))
+
+name2 = input("What is the name of other flatmate? ")
+days_in_house2 = int(input(f"How many did {name2} stay in the house during the bill period? "))
+
+the_bill = Bill(amount=bill_amount, period=pay_period)
+flatmate1 = Flatmate(name=name1, days_in_house=days_in_house1)
+flatmate2 = Flatmate(name=name2, days_in_house=days_in_house2)
+
+print(f"{flatmate1.name} pays: ", flatmate1.pays(bill=the_bill, flatmate2=flatmate2))
+print(f"{flatmate2.name} pays: ", flatmate2.pays(bill=the_bill, flatmate2=flatmate1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1=flatmate1, flatmate2=flatmate2, bill=the_bill)
